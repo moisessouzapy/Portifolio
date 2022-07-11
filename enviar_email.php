2be42f18280c7ff5f22fb3b2ff2b6038b7e55027
Moises Souza <moisessouza0204@gmailcom>
2022-07-11 01:36:00 -0300

    update css and html :up:

--- enviar_email.php
+++ enviar_email.php
@@ -1,40 +1,21 @@
-<?php
-  //Variáveis
-  $nome = $_POST['nome'];
-  $email = $_POST['email'];
-  $mensagem = $_POST['mensagem'];
-  $qtde = $_POST['qtde'];
-  $js = $_POST['js'];
-  $layout_sim = $_POST['layout_sim'];
-  $prazo = $_POST['prazo'];
-  $data_envio = date('d/m/Y');
-  $hora_envio = date('H:i:s');
+<?php 
 
-  //Compo E-mail
-  $arquivo = "
-    <html>
-      <p><b>Nome: </b>$nome</p>
-      <p><b>E-mail: </b>$email</p>
-      <p><b>Mensagem: </b>$mensagem</p>
-      <p><b>Quantidade de Páginas: </b>$qtde</p>
-      <p><b>Preciso de um script JS: </b>$js</p>
-      <p><b>Preciso de um layout: </b>$layout_sim</p>
-      <p><b>Prazo: </b>$prazo</p>
-      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
-    </html>
-  ";
-  
-  //Emails para quem será enviado o formulário
-  $destino = "moisessouza0204@gmail.com";
-  $assunto = "Contato pelo Portifólio";
+if(isset($_POST['email']) && !empty($_POST['email'])){
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $mensagem = addslashes($_POST['mensagem']);
 
-  //Este sempre deverá existir para garantir a exibição correta dos caracteres
-  $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
-
-  //Enviar
-  mail($destino, $assunto, $arquivo, $headers);
-  
-  echo "<meta http-equiv='refresh' content='10;URL=../index.html'>";
+    $to = "moisessouza0204@gmail.com";
+    $subject= "Contato - Programador Portifólio";
+    $body = "Nome: ".$nome. "\r\n".
+            "Email: ".$email. "\r\n".
+            "Mensagem: ".$mensagem;
+    $header = "From: moisessouza0204@gmail.com"."\r\n"."Reply-to:".$email."\r\n"."X=Mailer:PHP/".phpversion();
+    if(mail($to,$subject,$body,$header)){
+        echo("Email enviado com suceso!");
+    }else {
+        echo("Não pode ser enviado!");
+    }
+    header("Location: index.html");
+}
 ?>
